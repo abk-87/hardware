a016bd4164528b6b8df2ea44fbc271248f318a0a
Babken Abreyan <babkenabreyan@gmail.com>
2023-05-24 23:32:48 +0400

    edited

--- sum/tb.v
+++ sum/tb.v
@@ -3,7 +3,7 @@ module top();
 
 reg [31:0] in1;
 reg [31:0] in2;
-wire [31:0] out;
+wire [32:0] out;
 
 full_adder adder_test(.in1(in1), .in2(in2), .out(out));
 
@@ -23,6 +23,10 @@ initial begin
     $display("%d+%d=%d", in1, in2, out);
     in1 = 8624; 
     in2 = 1397; 
+    #1;
+    $display("%d+%d=%d", in1, in2, out);
+    in1 = 4294967295; 
+    in2 = 2161866544; 
     #1;
     $display("%d+%d=%d", in1, in2, out);
 end
